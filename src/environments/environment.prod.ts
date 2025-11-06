89055af4fedb6694fd6257d838952012f562e377
Adamo08 <omarelaaddam519@gmail.com>
2025-11-06 16:26:54 +0100

    Update production url in environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://adamo-smartfleet-backend-api-production.up.railway.app',
-  wsUrl: 'https://adamo-smartfleet-backend-api-production.up.railway.app/ws'
+  apiUrl: 'https://adamo-smartfleet-backend-api.up.railway.app',
+  wsUrl: 'https://adamo-smartfleet-backend-api.up.railway.app/ws'
 };
